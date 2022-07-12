92047aa7d7452943445f6d5863df87379489a082
ArhipovVS <Arhipof05@yandex.ru>
2022-07-12 21:05:29 +0700

    последняя цифра трехзначного числа

---
+++ Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// 7. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю цифру
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.Clear();
+Console.WriteLine ("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num<0)
+{
+    num=num*-1;
+}
+
+
+if (num > 99 && num < 1000)
+{
+    int resalt = num % 10;
+    Console.WriteLine(resalt);
+}
+else 
+{
+    Console.WriteLine ("Число не трехзначное");
+}
