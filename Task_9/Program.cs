04c9da55d7fcf115a41b0ab8b65f28762143fa00
ArhipovVS <Arhipof05@yandex.ru>
2022-07-13 00:30:31 +0700

    Task_9

---
+++ Task_9/Program.cs
@@ -0,0 +1,15 @@
+﻿int num = new Random ().Next (10,100);
+int firstNum = num % 10;
+int toNum = num /10;
+if (firstNum == toNum) 
+{
+    Console.WriteLine("цифры равны");
+}
+if (firstNum> toNum)
+{
+    Console.WriteLine($"{num} -> {firstNum}");
+}
+else 
+{
+    Console.WriteLine($"{num} -> {toNum}");
+} 
