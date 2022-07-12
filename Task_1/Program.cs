e17063f43f2f238ba334d39b0cd9eb724958220f
ArhipovVS <Arhipof05@yandex.ru>
2022-07-12 20:46:27 +0700

    исправление задач

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -23,39 +23,3 @@ else
   {
     Console.WriteLine("Первое число не квдрат второго");
   }
-
-
-Console.Write("Write a number N: ");
-int n = int.Parse(Console.ReadLine());
-
-int i = 0;
-
-if (n < 0)
-{
-     i = n;
-    n = -n;
-}
-else
-{
-   i = -n;
-}
-
-while (i <= n)
-{
-    Console.WriteLine(i);
-    i++;
-}
-
-
-int a = Convert.ToInt32(Console.ReadLine());
-int b = 100,
-    i = 0;
-
-while (i < 3){
-    b = a / b;
-    i++;
-}
-
-b = a / 100;
-
-Console.WriteLine(b);
