e8c563347b56abe4a9dc4d76c0c8d362afbc9a99
ArhipovVS <Arhipof05@yandex.ru>
2022-07-16 10:57:55 +0700

    задача про значи в номере четверти

---
+++ Task_18/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+
+
+Console.WriteLine("Введите номер четверти");
+string num = Console.ReadLine();
+string Quarte (string q)
+{   
+
+if (q == "1") return"xc >0  && yc >0";
+if (q == "2") return "xc < 0  && yc > 0";
+if (q == "3") return "xc < 0  && yc < 0";
+if (q == "3") return "xc > 0  && yc < 0";
+return "введены некорректные короодинвты";
+
+}
+
+string result = Quarte (num);
+Console.WriteLine (result);
