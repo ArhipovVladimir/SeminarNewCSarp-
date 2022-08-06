acebbc24ca245081f0eec219e205de2077a96c93
ArhipovVS <Arhipof05@yandex.ru>
2022-08-06 21:08:31 +0700

    задача32

---
+++ Task_32/Program.cs
@@ -0,0 +1,21 @@
+﻿void Print (int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i==0) Console.Write("[");
+        if (i < arr.Length-1) Console.Write(arr[i]+", ");
+        else Console.Write(arr[i]+"]");
+    }
+} 
+
+
+
+int [] array = {-4,-8,8,2};
+int [] arrayNeg = new int [array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    arrayNeg[i]=array[i]*-1;
+}
+Print(array);
+Console.Write("->");
+Print(arrayNeg);
