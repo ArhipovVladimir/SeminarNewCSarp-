31177de6876cd5f5dbafeffe7ec904b35576826f
ArhipovVS <Arhipof05@yandex.ru>
2022-08-10 09:46:13 +0700

    Revers

---
+++ Task_39/Program.cs
@@ -0,0 +1,45 @@
+﻿// Реверсия (поворот массива)
+
+
+
+int[] Fillarray(int site, int min, int max)
+{
+    int[] array = new int[site];
+    Random r = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = r.Next(min, max + 1);
+    }
+    return array;
+}
+
+void Printarray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
+
+void Reverse(int[] array)
+{
+    int size = array.Length;
+    int index1 = 0;
+    int index2 = size - 1;
+    while (index1 < index2) 
+    {
+        int obj = array [index1];
+        array[index1]=array[index2];
+        array[index2]=obj;
+        index1++;
+        index2--;
+    }
+}
+
+int[] array = Fillarray(5, 1, 9);
+Printarray(array);
+Reverse(array);
+Console.WriteLine();
+Printarray(array);
