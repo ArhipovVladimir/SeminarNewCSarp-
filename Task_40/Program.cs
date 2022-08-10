113063ab544cc7a577e4492fb5ad9bcb7d18eb66
ArhipovVS <Arhipof05@yandex.ru>
2022-08-10 10:15:27 +0700

    теория треугольника

---
+++ Task_40/Program.cs
@@ -0,0 +1,13 @@
+﻿// теорема треугольников
+string TestTeorem (int a1, int b1, int c1)
+{
+    if (a1 < b1+c1 && b1 < a1+c1 && c1 < a1+ b1) return "можно";
+    else return "нельзя";
+}
+
+
+Console.WriteLine("введите тричисла;");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Треугольник составить {TestTeorem(a, b, c)}");
