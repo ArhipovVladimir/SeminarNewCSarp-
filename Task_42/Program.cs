6853eaeb2a67e4605e38aa572d5920e5455ebaaa
ArhipovVS <Arhipof05@yandex.ru>
2022-08-10 12:38:34 +0700

    ToBin

---
+++ Task_42/Program.cs
@@ -0,0 +1,15 @@
+﻿// перевод числа из десетичной
+// ситсемы исчисления в двоичную
+void ToBin(int n)
+{
+    if (n == 0) return; 
+    ToBin(n/2);
+    Console.Write(n%2);
+    
+
+}
+
+
+Console.WriteLine("ведите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+ToBin(num);
