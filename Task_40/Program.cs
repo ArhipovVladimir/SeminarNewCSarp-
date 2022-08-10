d48e8917a5c26d60eaf35a8394a6b45249b58352
ArhipovVS <Arhipof05@yandex.ru>
2022-08-10 10:59:54 +0700

    терема треульников

--- Task_40/Program.cs
+++ Task_40/Program.cs
@@ -1,13 +1,22 @@
 ﻿// теорема треугольников
+
+
+int GetUseImput()
+{
+    Console.Write("введите число:");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
 string TestTeorem (int a1, int b1, int c1)
 {
     if (a1 < b1+c1 && b1 < a1+c1 && c1 < a1+ b1) return "можно";
     else return "нельзя";
+
 }
 
+int num1 = GetUseImput();
+int num2 = GetUseImput();
+int num3 = GetUseImput();
 
-Console.WriteLine("введите тричисла;");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
-int c = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Треугольник составить {TestTeorem(a, b, c)}");
+string result = TestTeorem(num1, num1 , num1);
+Console.WriteLine($"Треугольник составить {result}");
