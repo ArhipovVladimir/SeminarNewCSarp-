6a0ea9c336f3a16e9c5b12410d66060594c8ae34
ArhipovVS <Arhipof05@yandex.ru>
2022-08-14 18:54:23 +0700

    исправил

--- Task_48/Program.cs
+++ Task_48/Program.cs
@@ -3,7 +3,7 @@
     int [,] array = new int [m,n];
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
             array [i, j] = i+j;
         }
@@ -16,7 +16,7 @@ void PrintMatrix (int [,] array)
 
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
             if (j == 0) Console.Write ("[");
             if (j < array.GetLength(1) - 1) Console.Write ($"{array[i,j],  3},");
