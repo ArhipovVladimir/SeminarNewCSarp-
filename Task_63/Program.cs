287687dde915b3e6fea25a35daec619c3240f15b
ArhipovVS <Arhipof05@yandex.ru>
2022-08-21 21:38:57 +0700

    Task63

---
+++ Task_63/Program.cs
@@ -0,0 +1,16 @@
+﻿int GetUserInput()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNatural(int num)
+{
+    if (num==0) return;
+    PrintNatural (num-1);
+    Console.Write($"{num}, ");
+}
+
+
+Console.Write("Введите число");
+int num = GetUserInput();
+PrintNatural(num);
